e68dca9f43e2e06964950b4e7e255eb178d1f83f
Lawrence <lawrenslava1@gmail.com>
2018-03-20 14:57:45 +0300

    Add changes to js/bmi.js Will make the BMI html page issue the BMI result after calculations

--- js/bmi.js
+++ js/bmi.js
@@ -1,8 +1,10 @@
 var bmi=function(height,weight){
   result=weight/height;
   bmi=result/height;
+  return bmi;
 };
 
 var weight=parseInt(prompt("Enter your weight: "));
 var height=parseInt(prompt("Enter your height: "));
-alert("Your BMI is:",bmi(weight,height))
+final=bmi(weight,height);
+alert("Your BMI is:",bmi(weight,height));
