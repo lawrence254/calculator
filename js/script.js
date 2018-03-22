b02b8bf97bb974d308f8093a812594dd1b7ef410
Lawrence <lawrenslava1@gmail.com>
2018-03-22 10:55:40 +0300

    Add user interface(form) to the calculator app Change the mathematical function for division form % to / Re arrange the subtraction function from number2-number1 to number1-number2

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ var add=function(number1,number2){
 	return number1+number2;
   };
 var subtract=function(number1,number2){
-  return number2-number1;
+  return number1-number2;
 }
 var divide=function(number1,number2){
-  return number1%number2;
+  return number1/number2;
 }
 var multiply=function(number1,number2){
   return number1*number2;
