63e67a59420afc0ce4dba6a730cfbabd9ff159c8
Lawrence <lawrenslava1@gmail.com>
2018-03-22 11:46:50 +0300

    Add all the other math functions interfaces Add the js required to run all the functions

--- js/script.js
+++ js/script.js
@@ -16,6 +16,30 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-		$("#output").text(result);
-  })
+		$("#addoutput").text(result);
+  });
+	$("form#subtract").submit(function (event) {
+		event.preventDefault();
+		var number1=parseInt($(".num1").val());
+		var number2=parseInt($(".num2").val());
+		var result=subtract(number1,number2);
+		console.log(result);
+		$("#suboutput").text(result);
+	});
+	$("form#multiply").submit(function (event) {
+		event.preventDefault();
+		var number1=parseInt($(".mnum1").val());
+		var number2=parseInt($(".mnum2").val());
+		var result=multiply(number1,number2);
+		console.log(result);
+		$("#mutoutput").text(result);
+	});
+	$("form#divide").submit(function (event) {
+		event.preventDefault();
+		var number1=parseInt($(".dnum1").val());
+		var number2=parseInt($(".dnum2").val());
+		var result=divide(number1,number2);
+		console.log(result);
+		$("#divoutput").text(result);
+	});
 });
